916895e17bf39c7643497c191fd56f866c51a172
Shrey <shrey.dabhi23@gmail.com>
2020-07-05 12:08:34 +0530

    Update theme and deps
    - remove stale deps
    - change theme color
    - replace roboto with ubuntu

--- src/components/parallaxbackground.js
+++ src/components/parallaxbackground.js
@@ -6,7 +6,7 @@ import './ParallaxBackground.css';
 const ParallaxBackground = (props, context) => {
 
   return (
-    <div className="parallax" style={{ color: "#455A64" }}>
+    <div className="parallax" style={{ color: "#333333" }}>
       <div id="stars1" />
       <div id="stars2" />
       <div id="stars3" />
