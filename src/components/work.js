7f38781f186a079d8f0afb46ef511c68d7a702a2
sdabhi23 <shrey.dabhi23@gmail.com>
2021-02-15 22:39:15 +0530

    completed work experience

--- src/components/work.js
+++ src/components/work.js
@@ -15,37 +15,49 @@ const Work = () => {
           style={{
             fontFamily: "Markazi Text, cursive",
             fontSize: "3em",
-            margin: "20px",
+            margin: "20px 0",
           }}>
-          Currently Working at:
+          Currently:
         </p>
-        <img src={optum} />
+        <div id="employer">
+          <img src={optum} alt="Optum Logo"/>
+          <div>
+            <h2>Software Engineer @ Optum</h2>
+            <p>
+              Working on migration of legacy mainframe applications to big data
+              environments
+            </p>
+            <p>
+              <b>Tech Stack:</b> Hadoop, Hive, Spark, Scala, Python
+            </p>
+          </div>
+        </div>
       </div>
       <div>
         <p
           style={{
             fontFamily: "Markazi Text, cursive",
             fontSize: "3em",
-            margin: "20px",
+            margin: "20px 0",
           }}>
           Past Employers:
         </p>
         <div>
           <div id="past-employer">
-            <img src={hasura} />
-            <p>Hasura</p>
+            <img src={rbei} alt="RBEI Logo"/>
+            <p>Robert Bosch</p>
           </div>
           <div id="past-employer">
-            <img src={taaraka} />
-            <p>Taaraka</p>
-          </div>
-          <div id="past-employer">
-            <img src={ln} />
+            <img src={ln} alt="Let's Nurture Logo"/>
             <p>Let's Nurture</p>
           </div>
           <div id="past-employer">
-            <img src={rbei} />
-            <p>Robert Bosch</p>
+            <img src={taaraka} alt="Taaraka Logo"/>
+            <p>Taaraka</p>
+          </div>
+          <div id="past-employer">
+            <img src={hasura} alt="Hasura Logo"/>
+            <p>Hasura</p>
           </div>
         </div>
       </div>
