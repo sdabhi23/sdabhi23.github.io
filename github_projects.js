105dbb10317c936deb400bcffabb4cd80fc88949
shrey <shrey.dabhi23@gmail.com>
2023-11-20 03:37:41 +0530

    add the github projects structure and the footer

---
+++ github_projects.js
@@ -0,0 +1,20 @@
+fetch("https://portfolio-api.shreydabhi.dev/github_repos/", {
+  method: "GET",
+})
+  .then((response) => response.json())
+  .then((result) => {
+    console.log(result);
+    const githubProjects = document.getElementById("github-projects");
+    Object.values(result.data).forEach((repo) => {
+      var githubLink = document.createElement("a");
+      githubLink.href = repo.url;
+
+      var githubImage = document.createElement("img");
+      githubImage.src = repo.openGraphImageUrl;
+      githubImage.className = "github-card-img";
+
+      githubLink.appendChild(githubImage);
+      githubProjects.appendChild(githubLink);
+    });
+  })
+  .catch((error) => console.log("error", error));
